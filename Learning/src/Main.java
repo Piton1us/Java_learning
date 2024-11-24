1c3f4485598af40f217d30d30643340a258c7b95
Piton1us <89169196952@mail.ru>
2024-11-24 21:40:28 +0300

    Добавил несколькоуроков)

--- Learning/src/Main.java
+++ Learning/src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how GIGA IDE suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.security.KeyStore;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void  main(String[] args) {
+//        Третий урок
+//        System.out.println("Goida");
+
+///////////////////////////////////////////////////////
+//      Четвёртый урок: Переменные и типы данных
+
+//        int age = 20;
+//        System.out.println("Age: " + age + ".");
+
+//          byte age = 1;
+//          short num1 = 12;
+//          int num2 = 123;
+//          long num3 = 1234;
+
+        // byte - от -128 до 127 и занимает 1 байт.
+        // short - от -32768 до 32727, и занимает 2 байта.
+        // int - от - -2147483648 до 2147483647 и занимает 4 байта.
+        // long - от - -9 223 372 036 854 775 808 до 9 223 372 036 854 775 807 и занимает 8 байт
+
+//        float num4 = 4.323f;
+//        double num5 = 3.123213d;
+//
+//        char ch = 'd';
+//        String str = "based";
+//
+//        boolean isHappy = true;
+// ///////////////////////////////////////////////////
+
+        //Пятый урок: данные от пользователя и математические действия
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Name: ");
+
+//        String userNAme =  scan.nextLine();
+//        System.out.print("User name - " + userNAme);
+//        int num1 = scan.nextInt();
+//        boolean b = scan.nextBoolean();
+
+//        short num1 = 50, num2 = 10;
+//        int res = num1 + num2;
+//
+//        res += 10;
+//        res ++;
+//
+//        System.out.print(res);
+
+
     }
+
 }
